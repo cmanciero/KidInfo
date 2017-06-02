755b8d0fe6d8b065204d4ca517be24506d987be9
Christopher Manciero <christopher.manciero@sap.com>
2017-06-02 11:20:49 -0400

    More fields
    Added more fields to KidInfo screen, also added avatar button to select
    picture

--- KidInfo/KidInfoViewController.swift
+++ KidInfo/KidInfoViewController.swift
@@ -8,22 +8,27 @@
 
 import UIKit
 
-class KidInfoViewController: UIViewController {
+class KidInfoViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
     @IBOutlet weak var txtFirstName: UITextField!
-    @IBOutlet weak var btnAddUpdate: UIButton!
+    @IBOutlet weak var txtLastName: UITextField!
+    @IBOutlet weak var txtWeight: UITextField!
+    @IBOutlet weak var txtHeight: UITextField!
     @IBOutlet weak var btnDelete: UIButton!
-    
+    @IBOutlet weak var btnAvatar: UIButton!
+
     var kid: Kid? = nil;
+    var imagePicker = UIImagePickerController();
     
     override func viewDidLoad() {
-        super.viewDidLoad()
+        super.viewDidLoad();
+        
+        imagePicker.delegate = self;
         
         // check if kid exist
         if(kid != nil){
             txtFirstName.text = kid!.firstName;
-            
-            btnAddUpdate.setTitle("Update", for: .normal);
+            txtLastName.text = kid!.lastName;
             btnDelete.isHidden = false;
         }
     }
@@ -33,20 +38,35 @@ class KidInfoViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-    @IBAction func addTapped(_ sender: Any) {
-        // check if kid exists
+    @IBAction func saveTapped(_ sender: Any) {
         if(kid != nil){
             kid!.firstName = txtFirstName.text;
+            kid!.lastName = txtLastName.text;
         } else {
             let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext;
             
             let kid = Kid(context: context);
             kid.firstName = txtFirstName.text;
+            kid.lastName = txtLastName.text;
+            //            kid.height = txtHeight.text;
+            //            kid.weight = txtWeight.text;
         }
         
         (UIApplication.shared.delegate as! AppDelegate).saveContext();
         
         navigationController?.popViewController(animated: true);
+    }
+    @IBAction func avatarTapped(_ sender: Any) {
+        imagePicker.sourceType = .photoLibrary;
+        
+        present(imagePicker, animated: true, completion: nil);
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        let image = info[UIImagePickerControllerOriginalImage] as! UIImage;
+        
+        btnAvatar.setBackgroundImage(image, for: .normal);
+        imagePicker.dismiss(animated: true, completion: nil);
     }
     
     @IBAction func deleteTapped(_ sender: Any) {
