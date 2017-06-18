79167b6568daefa2bacbb2b8fe38853cf0cfec74
Christopher Manciero <christopher.manciero@sap.com>
2017-06-17 21:51:16 -0400

    Added charting :)
    First pass at putting charting into the application

--- KidInfo/WeightGrowthChartViewController.swift
+++ KidInfo/WeightGrowthChartViewController.swift
@@ -7,19 +7,54 @@
 //
 
 import UIKit
+import Charts
 
 class WeightGrowthChartViewController: UIViewController {
     var kid: Kid? = nil;
-
+    var months: [String]!;
+    
+    @IBOutlet weak var lineChart: LineChartView!
+    
+   
     override func viewDidLoad() {
-        super.viewDidLoad()
+        super.viewDidLoad();
 
         // Do any additional setup after loading the view.
+        months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
+        let unitsSold = [20.0, 4.0, 6.0, 3.0, 12.0, 16.0, 4.0, 18.0, 2.0, 4.0, 5.0, 4.0]
+        
+        setChart(dataPoints: months, values: unitsSold);
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func setChart(dataPoints: [String], values: [Double]){
+        lineChart.noDataText = "You need to provide data for the chart";
+        
+        var dataEntries: [ChartDataEntry] = [];
+        
+        for i in 0..<dataPoints.count {
+            let dataEntry = ChartDataEntry(x: Double(i), y: values[i]);
+            dataEntries.append(dataEntry)
+        }
+  
+        lineChart.backgroundColor = UIColor(red: 189/255, green: 195/255, blue: 199/255, alpha: 1)
+        lineChart.xAxis.labelPosition = .bottom;
+        // display values on xaxis
+        lineChart.xAxis.valueFormatter = IndexAxisValueFormatter(values: dataPoints);
+        lineChart.animate(xAxisDuration: 1.0, yAxisDuration: 1.0);
+        
+        let lineChartDataSet = LineChartDataSet(values: dataEntries, label: "Unites");
+        let lineChartData = LineChartData(dataSet: lineChartDataSet);
+        // set colors, array of colors to loop through for data point
+//        chartDataSet.colors = [UIColor(red: 230/255, green: 126/255, blue: 34/255, alpha: 1)];
+//        lineChartDataSet.colors = ChartColorTemplates.material();
+        lineChart.data = lineChartData;
+        // sets description text in lower right corner
+        lineChart.chartDescription?.text = "";
     }
     
 
