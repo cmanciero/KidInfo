f578d072dc45eb7a7696877ba0bc5a9709665e0f
Christopher Manciero <christopher.manciero@sap.com>
2017-07-22 22:24:31 -0400

    dropbox
    Added drop box api

---
+++ KidInfo/SettingsViewController.swift
@@ -0,0 +1,48 @@
+//
+//  SettingsViewController.swift
+//  KidInfo
+//
+//  Created by i814935 on 7/22/17.
+//  Copyright © 2017 Chris Manciero. All rights reserved.
+//
+
+import UIKit
+import SwiftyDropbox
+
+class SettingsViewController: UIViewController {
+
+    @IBOutlet weak var btnDone: UIBarButtonItem!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func doneTapped(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil);
+    }
+    @IBAction func authorizeDropbox(_ sender: Any) {
+        DropboxClientsManager.authorizeFromController(UIApplication.shared,
+                                                      controller: self,
+                                                      openURL: { (url: URL) -> Void in
+                                                        UIApplication.shared.open(url);
+        });
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
