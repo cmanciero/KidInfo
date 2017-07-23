784b175114223cb03680bf3351d17ddfab11d0e5
Christopher Manciero <christopher.manciero@sap.com>
2017-07-23 10:11:20 -0400

    Removing dropbox cocoa pods

--- KidInfo/AppDelegate.swift
+++ KidInfo/AppDelegate.swift
@@ -8,7 +8,6 @@
 
 import UIKit
 import CoreData
-import SwiftyDropbox
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -18,21 +17,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-        DropboxClientsManager.setupWithAppKey("t8f16412v21o79p");
-        return true
-    }
-    
-    func application(_ app: UIApplication, open url: URL, options: [UIApplicationOpenURLOptionsKey : Any] = [:]) -> Bool {
-        if let authResult = DropboxClientsManager.handleRedirectURL(url) {
-            switch authResult {
-            case .success:
-                print("Success! User is logged into Dropbox.")
-            case .cancel:
-                print("Authorization flow was manually canceled by user!")
-            case .error(_, let description):
-                print("Error: \(description)")
-            }
-        }
         return true
     }
 
